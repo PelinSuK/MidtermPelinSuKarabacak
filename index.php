69e717289d274c27d2a2119dee3891a2b891dcfd
PelinSuK <pelin.su.k@hotmail.com>
2021-04-10 10:48:48 +0300

    changed index

--- index.php
+++ index.php
@@ -3,3 +3,25 @@
 Welcome Web Programming2 Midterm
 </p>
 ?> 
+<!DOCTYPE html>
+<html lang ='en'>
+<head>
+	<title>Midterm </title>
+	<meta charset='utf-8'>
+</head>
+<body>
+
+<h1>Automobile</h1>
+
+<form method='GET' action='Automobile.php'>
+
+	<label> Licence Plate: <input type='text' name='licence'></input></label>
+	<label> Brand: <input type='text' name='brand'></input></label>
+	<label> Model: <input type='text' name='model'></input></label>
+	<label> Model Year: <input type='text' name='modelYear'></input></label>
+	<label> Color: <input type='text' name='color'></input></label>
+	<input type='submit' value='Submit'>
+	
+</form>
+</body>
+</html>
